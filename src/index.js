1ca5ac6ea4323337ca78b76837b99bff61ef1bd9
Ivan Babak <babak.john@gmail.com>
2018-06-06 00:52:44 -0700

    Disable debug mode by port on local (non-production)
    - Closer to production by default. Use `?debug` to enable tweak controls on local like on production.
    - Keeping the debug mode by port code in place for quick re-enabling if needed.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import SompylasarWebsiteVRRoot from './SompylasarWebsiteVRRoot';
 
 
-const isDebugPort = (!!window.location.port && parseInt(window.location.port, 10) !== 80);
+// NOTE(@sompylasar): Disable debug mode by port on local (non-production). Closer to production by default.
+// NOTE(@sompylasar): Keeping the debug mode by port code in place for quick re-enabling if needed.
+const isDebugPort = false && (!!window.location.port && parseInt(window.location.port, 10) !== 80);
 const isDebugQueryString = /(^|[?&])debug([=]|$)/i.test(window.location.search);
 const isDebug = (isDebugPort || isDebugQueryString);
 
