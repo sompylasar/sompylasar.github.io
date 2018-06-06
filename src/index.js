9dc9ef757a2a5fd33711278ddfb3cbeee6e6538e
Ivan Babak <babak.john@gmail.com>
2018-06-06 00:47:37 -0700

    Add query string `?debug` to enable tweak controls in production

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import SompylasarWebsiteVRRoot from './SompylasarWebsiteVRRoot';
 
 
-const isDebug = (!!window.location.port && parseInt(window.location.port, 10) !== 80);
+const isDebugPort = (!!window.location.port && parseInt(window.location.port, 10) !== 80);
+const isDebugQueryString = /(^|[?&])debug([=]|$)/i.test(window.location.search);
+const isDebug = (isDebugPort || isDebugQueryString);
 
 const rootEl = document.getElementById('sompylasar-website-vr');
 if (rootEl) {
